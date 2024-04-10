5bc82bd4f257dba5db46a8871cafe42e5f715990
Hristo Yonkov <hristopturs@gmail.com>
2024-04-10 13:10:58 +0300

    Create DataTypesAndVariables.java

---
+++ Youtube-Tutorials/BroCode/Data-Types-Variables-User-Inputs/DataTypesAndVariables.java
@@ -0,0 +1,14 @@
+class DataTypesAndVariables {
+    public static void main(String[] args) {
+        int a = 123;
+        long b = 12312312312312312L; // L or l at the end of a number by convention!
+        byte c = 127; // 127 is maximum -128 is mimimum!
+        float d = 3.123456F; // F or f at the end of a number by convention 6 digits precision!
+        double e = 3.123456789012345; // F or f not needed and have more precision with 15 digits!
+        boolean f = true;
+        char symbol = '@';
+        String name = "Hristo"; // Referent type of data!
+
+        System.out.println("My number is: " + name);
+    }
+}
