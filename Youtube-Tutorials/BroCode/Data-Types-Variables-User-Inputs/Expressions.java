04409ac04ed853d1c0b0d0fc143626756b1a88ae
Hristo Yonkov <hristopturs@gmail.com>
2024-04-10 13:56:04 +0300

    Create Expressions.java

---
+++ Youtube-Tutorials/BroCode/Data-Types-Variables-User-Inputs/Expressions.java
@@ -0,0 +1,10 @@
+public class Expressions {
+    public static void main(String[] args) {
+        double friends = 10;
+        // int friends = 10;
+        // friends = friends / 3; // will return 3 cause the variable is declared as int!
+        friends = friends / 3;
+
+        System.out.println(friends);
+    }
+}
