4efb5a33e8897253e8c135bf7cf860aeb613a581
Hristo Yonkov <hristopturs@gmail.com>
2024-04-13 13:25:49 +0300

    Create MathClass.java

---
+++ Youtube-Tutorials/BroCode/GUIIntro-MathClass-RandomValues/MathClass.java
@@ -0,0 +1,16 @@
+public class MathClass {
+    public static void main(String[] args) {
+        double x = 3.14;
+        double y = -10;
+
+        // double z = Math.max(x, y);
+        // double z = Math.min(x, y);
+        // double z = Math.abs(y);
+        // double z = Math.sqrt(x);
+        // double z = Math.round(x);
+        // double z = Math.ceil(x);
+        double z = Math.floor(x);
+
+        System.out.println(z);
+    }
+}
