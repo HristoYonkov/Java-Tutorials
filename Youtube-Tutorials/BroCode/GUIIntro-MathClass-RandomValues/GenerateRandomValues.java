f8010e6366283c07d841bb2210c01e5b15231382
Hristo Yonkov <hristopturs@gmail.com>
2024-04-14 10:52:16 +0300

    Update GenerateRandomValues.java

--- Youtube-Tutorials/BroCode/GUIIntro-MathClass-RandomValues/GenerateRandomValues.java
+++ Youtube-Tutorials/BroCode/GUIIntro-MathClass-RandomValues/GenerateRandomValues.java
@@ -3,7 +3,9 @@ import java.util.Random;
 public class GenerateRandomValues {
     public static void main(String[] args) {
         Random random = new Random();
-        int x = random.nextInt();
+        // int x = random.nextInt(6) + 1;
+        // double x = random.nextDouble();
+        boolean x = random.nextBoolean();
 
         System.out.println(x);
     }
