930f007723d4cb212326808c0625d306ea66fd6e
Hristo Yonkov <hristopturs@gmail.com>
2024-04-15 15:05:39 +0300

    Create FinalKeyword.java

---
+++ Youtube-Tutorials/BroCode/5.Printf-FinalKeyword/FinalKeyword.java
@@ -0,0 +1,8 @@
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final double pi = 3.14159;
+        // pi = 5;
+        // With "final" keyword variables can no longer be changed!
+        System.out.println(pi);
+    }
+}
