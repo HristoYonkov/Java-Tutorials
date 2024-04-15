03cc4c39b442eafd5f456d3f006a62d537442a1e
Hristo Yonkov <hristopturs@gmail.com>
2024-04-15 23:37:42 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/OOP/Main.java
+++ Youtube-Tutorials/BroCode/OOP/Main.java
@@ -8,22 +8,6 @@ public class Main {
         System.out.println(car1.model);
 
         car1.drive();
-        car1.brake();
+        car2.brake();
     }
 }
-
-// class Car {
-//     String brand = "Mitsubishi";
-//     String model = "Lancer";
-//     int year = 2020;
-//     String color = "blue";
-//     double price = 10.000;
-
-//     void drive() {
-//         System.out.println("Driving...");
-//     }
-
-//     void brake() {
-//         System.out.println("Breaking...");
-//     }
-// }
