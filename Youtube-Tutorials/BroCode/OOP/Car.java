136ee7f30ba5e95766ab6d1f53ba774fa8543dfa
Hristo Yonkov <hristopturs@gmail.com>
2024-04-15 20:59:25 +0300

    Create Car.java

---
+++ Youtube-Tutorials/BroCode/OOP/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String brand = "Mitsubishi";
+    String model = "Lancer";
+    int year = 2020;
+    String color = "blue";
+    double price = 10.000;
+
+    void drive() {
+        System.out.println("Driving...");
+    }
+
+    void brake() {
+        System.out.println("Breaking...");
+    }
+}
