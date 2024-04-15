d662fd9bb77b5214f606ee5e2c36d8f3aef117d0
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 00:41:03 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/OOP/Main.java
+++ Youtube-Tutorials/BroCode/OOP/Main.java
@@ -1,11 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        Car car2 = new Car();
+        Car car1 = new Car("Mitsubishi", "lancer", 2010, "White", 10.00);
+        Car car2 = new Car("Toyota", "Supra", 2010, "Blue", 30.00);
 
         System.out.println(car1.brand);
-        System.out.println(car1.model);
+        System.out.println(car2.model);
 
         car1.drive();
         car2.brake();
