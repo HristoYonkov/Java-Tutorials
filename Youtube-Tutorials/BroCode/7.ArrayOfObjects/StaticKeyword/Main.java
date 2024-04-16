cdea38c5c03c5731915e60867f2f3ff93504688a
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 17:02:48 +0300

    Create Main.java

---
+++ Youtube-Tutorials/BroCode/7.ArrayOfObjects/StaticKeyword/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // static = Modifier. A single coppy of a variable/method is created and shared.
+        // The class "owns" the static member.
+
+        Friend friend1 = new Friend("Pavel");
+        Friend friend2 = new Friend("Mitko");
+        Friend friend3 = new Friend("Strahil");
+
+        System.out.println(Friend.numberOfFriends);
+        Friend.countFriends();
+    }
+}
