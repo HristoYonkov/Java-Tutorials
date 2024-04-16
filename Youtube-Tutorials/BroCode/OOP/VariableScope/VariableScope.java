787555f11a9bb17d745e518b4915f653e7e3366c
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 13:07:18 +0300

    Create VariableScope.java

---
+++ Youtube-Tutorials/BroCode/OOP/VariableScope/VariableScope.java
@@ -0,0 +1,8 @@
+public class VariableScope {
+    public static void main(String[] args) {
+        // local varaible = Declared inside a method and is visible only to that method.
+        // global variable = Declare outside a method, but within a class visible to all parts of a class.
+
+        
+    }
+}
