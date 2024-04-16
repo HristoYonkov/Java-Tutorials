75557c0299d0a47068131c958ca040f387402f58
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 14:40:09 +0300

    Create CarNew.java

---
+++ Youtube-Tutorials/BroCode/OOP-VariableScope-OlConstructors-ToStringMethod/ToStringMethod/CarNew.java
@@ -0,0 +1,11 @@
+public class CarNew {
+    String brand = "Mitsubishi";
+    String model = "Lancer";
+    String year = "1990";
+    String color = "Red";
+
+    public String toString() {
+        String myString = brand+"\n"+model+"\n"+year+"\n"+color;
+        return myString;
+    }
+}
