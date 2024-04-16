822b6972adf9c0634801b1ab2f509a6e78a96992
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 13:57:41 +0300

    Update Pizza.java

--- Youtube-Tutorials/BroCode/OOP/OverloadedConstructors/Pizza.java
+++ Youtube-Tutorials/BroCode/OOP/OverloadedConstructors/Pizza.java
@@ -1,7 +1,28 @@
 
 public class Pizza {
+    // Overloaded constructors are like overloaded methods.
+    Pizza(String bread, String sauce, String cheese, String topping, int price) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+        this.price = price;
+    }
+
+    Pizza(String bread, String sauce, String cheese) {
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+    }
+
     Pizza() {
 
     }
-    
+
+    String bread;
+    String sauce;
+    String cheese;
+    String topping;
+    int price;
+
 }
