8594595150bac54b8b7df92c6728b3f49d839f2c
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 13:57:39 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/OOP/OverloadedConstructors/Main.java
+++ Youtube-Tutorials/BroCode/OOP/OverloadedConstructors/Main.java
@@ -1,6 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza();
+        // Pizza pizza = new Pizza("Italian","tomato","mozzerella","chili",15);
+        Pizza pizza = new Pizza("Italian","tomato","mozzerella");
+        // Pizza pizza = new Pizza();
+
+        System.out.println("Here are the ingredients for your pizza:");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
+        System.out.println(pizza.price);
     }
 }
