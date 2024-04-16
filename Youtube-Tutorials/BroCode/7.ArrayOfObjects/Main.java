e1d6c3cc4ebf07028edcd93e41ea6209c9447e47
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 15:15:12 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/7.ArrayOfObjects/Main.java
+++ Youtube-Tutorials/BroCode/7.ArrayOfObjects/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         
+        // Food[] refrigerator = new Food[3];
+
+        Food pizza = new Food("Pizza");
+        Food burger = new Food("Burger");
+        Food duner = new Food("Duner");
+
+        Food[] refrigerator = { pizza, burger, duner };
+
+        // refrigerator[0] = pizza;
+        // refrigerator[1] = burger;
+        // refrigerator[2] = duner;
+
+        System.out.println(refrigerator[0].name);
+        System.out.println(refrigerator[1].name);
+        System.out.println(refrigerator[2].name);
     }
 }
