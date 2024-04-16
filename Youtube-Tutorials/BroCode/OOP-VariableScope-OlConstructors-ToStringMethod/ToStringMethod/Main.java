b475d4a6520689a7d5dc7d42bba90520a6f621f3
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 14:40:12 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/OOP-VariableScope-OlConstructors-ToStringMethod/ToStringMethod/Main.java
+++ Youtube-Tutorials/BroCode/OOP-VariableScope-OlConstructors-ToStringMethod/ToStringMethod/Main.java
@@ -4,6 +4,12 @@ public class Main {
         // toString() = Special method that all objects inherit, that returns a string that
         // "textually represents" an object. Can be used both implicitly and explicitly.
 
-        Car car = new Car();
+        // Normally toString() shows address of the "car" object that you can find in your computer memmory.
+        // Todo: Check about method overriding!
+        CarNew car = new CarNew();
+
+        System.out.println(car.brand);
+        System.out.println(car);
+        System.out.println(car.toString()); // toString() was overrided!
     }
 }
