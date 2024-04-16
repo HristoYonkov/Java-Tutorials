41bb2c70d718db07b39fce1905a5cbb2139c4af3
Hristo Yonkov <hristopturs@gmail.com>
2024-04-16 18:06:35 +0300

    Create Vehicle.java

---
+++ Youtube-Tutorials/BroCode/8.Inheritance/Inheritance/Vehicle.java
@@ -0,0 +1,17 @@
+public class Vehicle {
+    // The Vehicle class is superclass also known as parent class.
+    
+    Vehicle() {
+
+    }
+
+    double speed;
+
+    void go() {
+        System.out.println("This vehicle is moving..");
+    }
+
+    void stop() {
+        System.out.println("This vehicle stops.");
+    }
+}
