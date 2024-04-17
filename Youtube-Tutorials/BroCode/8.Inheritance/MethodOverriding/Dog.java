83e9bd0b3fbbfff86a980c9bf0e83d8524b8f355
Hristo Yonkov <hristopturs@gmail.com>
2024-04-17 13:25:51 +0300

    Create Dog.java

---
+++ Youtube-Tutorials/BroCode/8.Inheritance/MethodOverriding/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal {
+    
+    // "speak()" method within the "Dog" class is considered the 'overriding' method.
+    void speak() {
+        System.out.println("The dog *barks*..");
+    }
+}
