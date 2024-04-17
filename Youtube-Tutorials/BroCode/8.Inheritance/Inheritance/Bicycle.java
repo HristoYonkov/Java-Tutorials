39a9e7f6a4c7ba05bfbd6d2818a05b50e3f3c6ea
Hristo Yonkov <hristopturs@gmail.com>
2024-04-17 12:57:20 +0300

    Update Bicycle.java

--- Youtube-Tutorials/BroCode/8.Inheritance/Inheritance/Bicycle.java
+++ Youtube-Tutorials/BroCode/8.Inheritance/Inheritance/Bicycle.java
@@ -1,3 +1,10 @@
 public class Bicycle extends Vehicle {
     // Bicycle class is subclass of the Vehicle Class. Also known as a child class.
+    
+    Bicycle(String name) {
+        this.name = name;
+    }
+
+    int wheel = 2;
+    int pedals = 2;
 }
