93b0adbfc742d6605837c6a1433e928fcb813c59
Hristo Yonkov <hristopturs@gmail.com>
2024-04-17 13:25:50 +0300

    Create Animal.java

---
+++ Youtube-Tutorials/BroCode/8.Inheritance/MethodOverriding/Animal.java
@@ -0,0 +1,7 @@
+public class Animal {
+    
+    // "speak()" method within the "Animal" class is considered the 'overridden' method.
+    void speak() {
+        System.out.println("The animal speaks..");
+    }
+}
