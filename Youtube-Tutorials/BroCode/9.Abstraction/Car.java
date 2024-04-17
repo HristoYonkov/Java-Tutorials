6de8d96430fb253341ea6d0e823d16a48270e7ab
Hristo Yonkov <hristopturs@gmail.com>
2024-04-17 16:38:18 +0300

    Create Car.java

---
+++ Youtube-Tutorials/BroCode/9.Abstraction/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle{
+    // If parent class does not have such a method error will be thrown if "@Override" is set.
+    // Try to comment the "Vehicle" class "go()" method.
+    
+    @Override // Explicitly overrides method "go()" if parent class have it.
+    void go() {
+        System.out.println("Car is moving..");
+    }
+}
