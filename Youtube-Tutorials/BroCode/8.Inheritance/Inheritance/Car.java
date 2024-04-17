c0d666cc2e22335ff87c5e940c64370e14f1b80b
Hristo Yonkov <hristopturs@gmail.com>
2024-04-17 12:57:22 +0300

    Update Car.java

--- Youtube-Tutorials/BroCode/8.Inheritance/Inheritance/Car.java
+++ Youtube-Tutorials/BroCode/8.Inheritance/Inheritance/Car.java
@@ -1,3 +1,10 @@
 public class Car extends Vehicle {
     // Car class is subclass of the Vehicle Class. Also known as a child class.
+    
+    Car(String name) {
+        this.name = name;
+    }
+
+    int wheels = 4;
+    int doors = 4;
 }
