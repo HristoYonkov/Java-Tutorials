ed8db2e7e7cc9dce23a38bde648edc5f1cf67431
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 13:26:25 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/10.Encapsulation/Main.java
+++ Youtube-Tutorials/BroCode/10.Encapsulation/Main.java
@@ -3,14 +3,16 @@ public class Main {
         // Encapsulation = Attributes of a class will be hidden or private
         // can be accessed only through methods (getters & setters)
         // You should make attributes private if you don't have a reason to make
-        //  them public or protected.
+        // them public or protected.
 
         Car car = new Car("Honda", "Civic", 2020);
 
-        car.getBrand();
-        // System.out.println(car.getModel);
-        // System.out.println(car.getYear);
-
-        // car.setYear(2022);
+        System.out.println(car.getBrand());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+        
+        car.setYear(2022);
+        
+        System.out.println(car.getYear());
     }
 }
