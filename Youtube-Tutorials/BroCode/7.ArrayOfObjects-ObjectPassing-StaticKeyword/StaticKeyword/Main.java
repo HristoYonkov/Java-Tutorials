53cb34fdde1a60f924c87c6e3062954bb8b111af
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 12:03:20 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/7.ArrayOfObjects-ObjectPassing-StaticKeyword/StaticKeyword/Main.java
+++ Youtube-Tutorials/BroCode/7.ArrayOfObjects-ObjectPassing-StaticKeyword/StaticKeyword/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        // static = Modifier. A single coppy of a variable/method is created and shared.
+        // static = Modifier. A single coppy of a variable/method is created and shared. This modifier stays for the class only!
         // The class "owns" the static member.
 
         Friend friend1 = new Friend("Pavel");
