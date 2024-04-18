3fd9ce7723ed0d606d7ca327a5f4628e2b0a84e4
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 10:36:48 +0300

    Update Asub.java

--- Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package2/Asub.java
+++ Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package2/Asub.java
@@ -1,6 +1,15 @@
 package AccessModifiers.package2;
+
 import AccessModifiers.package1.*;
 
-public class Asub {
-    
+public class Asub extends A {
+    // 
+    // public static void main(String[] args) {
+    // Asub asub = new Asub();
+    // System.out.println(asub.protectedMessage); // protected modifier
+    // }
+
+    // protected modifier = Something taht is protected is accessible to a different
+    // class in a different package, as long as that class is subclass of whatever class
+    // contains that protected member.
 }
