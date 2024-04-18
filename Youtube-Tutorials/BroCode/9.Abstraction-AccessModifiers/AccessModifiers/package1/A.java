d3cd82939458251a53944684e0ebee9f6bcbc691
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 10:36:41 +0300

    Update A.java

--- Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package1/A.java
+++ Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package1/A.java
@@ -1,5 +1,14 @@
 package AccessModifiers.package1;
+import AccessModifiers.package2.*;
 
 public class A {
-    
+    public static void main(String[] args) {
+        // protected String protectedMessage = "This is protected"; // protected modifier
+        B b = new B();
+        // System.out.println(b.privateMessage); // private modifier
+
+        C c = new C();
+        // System.out.println(c.defaultMessage); // default modifier
+        System.out.println(c.publicMessage); // public modifier
+    }
 }
