4e9f1af8cb5159d40d316729806b59ea4630f354
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 15:37:41 +0300

    Update Car.java

--- Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/Abstraction/Car.java
+++ Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/Abstraction/Car.java
@@ -1,4 +1,4 @@
-public class Car extends Vehicle{
+public class Car extends Vehicle {
     // If parent class does not have such a method error will be thrown if "@Override" is set.
     // Try to comment the "Vehicle" class "go()" method.
     
