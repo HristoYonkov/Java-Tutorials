92afee085a56897a33ca5b117f88f515b2b96b74
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 11:45:52 +0300

    Update A.java

--- Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package1/A.java
+++ Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package1/A.java
@@ -1,5 +1,5 @@
-package AccessModifiers.package1;
-import AccessModifiers.package2.*;
+package package1;
+import package2.*;
 
 public class A {
     public static void main(String[] args) {
@@ -12,3 +12,15 @@ public class A {
         System.out.println(c.publicMessage); // public modifier
     }
 }
+// "default" modifier = Anything using the default access modifier is only visible
+// to anything within the same package (available only for "package2").
+
+// "public" modifier = Anything that is public is available or visible to other packages.
+
+// "protected" modifier = Something taht is protected is accessible to a different
+// class in a different package, as long as that class is subclass of whatever class
+// contains that protected member.
+
+// "private" modifier = Something that is private is only visiible to the class that
+// it contains and in nothing else even if it's within the same package!
+
