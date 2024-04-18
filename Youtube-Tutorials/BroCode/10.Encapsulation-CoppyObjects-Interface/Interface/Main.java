e6609d21bea9ca5bd3fccd6881bf3a4d76c7fada
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 15:15:50 +0300

    Create Main.java

---
+++ Youtube-Tutorials/BroCode/10.Encapsulation-CoppyObjects-Interface/Interface/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // interfane = A template that can be applied to a class.
+        // similar to inheritance, but specified what a class has/must do.
+        // Classes can apply more than one interface, inheritance is limited
+        // to one direct super class.
+
+        Rabbit rabbid = new Rabbit();
+        rabbid.flee();
+
+        System.out.println();
+
+        Hawk hawk = new Hawk();
+        hawk.hunt();
+
+        System.out.println();
+
+        Fish fish = new Fish();
+        fish.flee();
+        fish.hunt();
+    }
+}
