0ec02743ec3f74c044c25a526ad61f3054048ec2
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 10:36:45 +0300

    Update B.java

--- Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package1/B.java
+++ Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package1/B.java
@@ -1,5 +1,8 @@
 package AccessModifiers.package1;
+import AccessModifiers.package2.*;
 
 public class B {
-    
+    // private modifier = Something that is private is only visiible to the class that
+    // it contains and in nothing else even if it's within the same package!
+    private String privateMessage = "This is private"; // private modifier
 }
