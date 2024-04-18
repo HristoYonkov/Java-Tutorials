ff3daf8c4c755aeb253dd69176560c50711fc520
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 10:36:53 +0300

    Update C.java

--- Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package2/C.java
+++ Youtube-Tutorials/BroCode/9.Abstraction-AccessModifiers/AccessModifiers/package2/C.java
@@ -1,5 +1,9 @@
 package AccessModifiers.package2;
+import AccessModifiers.package1.*;
 
 public class C {
-    
+    String defaultMessage = "This is a default"; // default modifier
+    public String publicMessage = "This is public"; // public modifier
+    protected String protectedMessage = "This is protected"; // protected modifier
+    private String privateMessage = "This is private"; // private modifier
 }
