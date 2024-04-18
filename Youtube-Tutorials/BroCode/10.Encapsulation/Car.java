a1804c734870b5ee57caa0403c03fe9f9e40187d
Hristo Yonkov <hristopturs@gmail.com>
2024-04-18 13:09:06 +0300

    Create Car.java

---
+++ Youtube-Tutorials/BroCode/10.Encapsulation/Car.java
@@ -0,0 +1,33 @@
+public class Car {
+    Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    private String brand;
+    private String model;
+    private int year;
+
+    // getters
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getYear() {   
+        return year;
+    }
+
+    // setters
+    // public void setBrand(String brand) {
+    //     this.brand = brand;
+    // }
+    // public void setModel(String model) {
+    //     this.model = model;
+    // }
+    // public void setYear(int year) {
+    //     this.year = year;
+    // }
+}
