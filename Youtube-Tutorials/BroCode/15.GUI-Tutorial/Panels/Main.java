9a3cf1b122d2c9d25ecaeb2ffc84d41a5a9dcbdd
Hristo Yonkov <hristopturs@gmail.com>
2024-04-22 00:34:12 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/15.GUI-Tutorial/Panels/Main.java
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/Panels/Main.java
@@ -11,6 +11,14 @@ public class Main {
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0, 0, 250, 250);
 
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -18,5 +26,7 @@ public class Main {
         frame.setVisible(true);
 
         frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
     }
 }
