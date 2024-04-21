58be41ba86d50cff2935a9ac1463e00476e14ad8
Hristo Yonkov <hristopturs@gmail.com>
2024-04-21 12:39:25 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/14.Audio/Main.java
+++ Youtube-Tutorials/BroCode/14.Audio/Main.java
@@ -1,8 +1,48 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import javax.sound.sampled.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+
         File file = new File("movement-200697.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        // Attempting to paly in BG thread but our program is not waiting for BG
+        // threads..
+        // As soon as you run the code, it will be terminated.
+        // We need to keep our program open and running.
+
+        String response = "";
+
+        while (!response.equals("Q")) {
+            System.out.println("P = play, S = stop, R = reset, Q = quit");
+            System.out.println("Enter your choise: ");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case ("P"): clip.start();
+                    break;
+                case ("S"): clip.stop();
+                    break;
+                case ("R"): clip.setMicrosecondPosition(0);
+                    break;
+                case ("Q"): clip.close();
+                    break;
+                default: System.out.println("Not a valid response!");
+                    break;
+            }
+        }
+
+        System.out.println("Audio-Player Closed!");
     }
 }
