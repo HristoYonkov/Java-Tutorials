83a68faf6c36bfb8bd5dceff31140234f5fd75bf
Hristo Yonkov <hristopturs@gmail.com>
2024-04-21 17:47:00 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/GUI/GUI-Tutorial/src/Main.java
+++ Youtube-Tutorials/BroCode/GUI/GUI-Tutorial/src/Main.java
@@ -1,6 +1,29 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        
-        
+    /*
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame title goes here.."); // Sets title of frame.
+        frame.setSize(450, 450); // Sets "x" and "y" dimensions of frame.
+        // frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); // Default condition on X button.
+        // frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); // Do nothing on X button.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit out of app with X button.
+        frame.setResizable(false); // Prevents frame from resizing.
+
+        frame.setVisible(true); // make rame visible.
+
+        ImageIcon image = new ImageIcon("C:/Users/Hristo/Desktop/Java-Tutorials/Youtube-Tutorials/BroCode/GUI/GUI-Tutorial/lib/wolf.png"); // Creates an ImageIcon.
+        frame.setIconImage(image.getImage()); // Change icon frame.
+        // frame.getContentPane().setBackground(Color.green); // Change color of background.
+        // frame.getContentPane().setBackground(new Color(0x000000)); // Hexademical colours.
+        frame.getContentPane().setBackground(new Color(0, 255, 0));
+    */
+
+        // MyFrame myFrame = new MyFrame();
+        new MyFrame();
     }
 }
