376cf066e085b981d35833af8c1f913cdf6260bf
Hristo Yonkov <hristopturs@gmail.com>
2024-04-22 17:50:28 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/15.GUI-Tutorial/BorderLAyout/Main.java
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/BorderLAyout/Main.java
@@ -17,6 +17,8 @@ public class Main {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
+        // new BorderLayout() = Changes the way panels should be displayed
+        // with setPreferredSize(new Dimension()).
         frame.setLayout(new BorderLayout(10, 10)); // Sets margin of the panels.
         frame.setVisible(true);
 
@@ -39,33 +41,40 @@ public class Main {
         rightPanel.setPreferredSize(new Dimension(100, 100));
         centerPanel.setPreferredSize(new Dimension(100, 100));
 
-        // ------ Sub Panels ------------------
+        // ------ Sub Panels ----------------------
         JPanel topPanel1 = new JPanel();
         JPanel botPanel2 = new JPanel();
         JPanel leftPanel3 = new JPanel();
         JPanel rightPanel4 = new JPanel();
         JPanel centerPanel5 = new JPanel();
 
-        topPanel1.setBackground(Color.red);
-        botPanel2.setBackground(Color.green);
-        leftPanel3.setBackground(Color.yellow);
-        rightPanel4.setBackground(Color.magenta);
-        centerPanel5.setBackground(Color.blue);
+        topPanel1.setBackground(Color.black);
+        botPanel2.setBackground(Color.darkGray);
+        leftPanel3.setBackground(Color.gray);
+        rightPanel4.setBackground(Color.lightGray);
+        centerPanel5.setBackground(Color.white);
 
-        // In following cases you can change only thickness of the panels.
-        topPanel.setPreferredSize(new Dimension(100, 100));
-        botPanel.setPreferredSize(new Dimension(100, 100));
-        leftPanel.setPreferredSize(new Dimension(100, 100));
-        rightPanel.setPreferredSize(new Dimension(100, 100));
-        centerPanel.setPreferredSize(new Dimension(100, 100));
-        // ----- Sub Panels ----------------
+        // Add new layout manager to the center panel
+        // which is acting as a container fo the new inner panels.
+        centerPanel.setLayout(new BorderLayout());
+
+        topPanel1.setPreferredSize(new Dimension(50, 50));
+        botPanel2.setPreferredSize(new Dimension(50, 50));
+        leftPanel3.setPreferredSize(new Dimension(50, 50));
+        rightPanel4.setPreferredSize(new Dimension(50, 50));
+        centerPanel5.setPreferredSize(new Dimension(50, 50));
+
+        centerPanel.add(topPanel1, BorderLayout.NORTH);
+        centerPanel.add(botPanel2, BorderLayout.SOUTH);
+        centerPanel.add(leftPanel3, BorderLayout.WEST);
+        centerPanel.add(rightPanel4, BorderLayout.EAST);
+        centerPanel.add(centerPanel5, BorderLayout.CENTER);
+        // ----- Sub Panels ------------------------
 
         frame.add(topPanel, BorderLayout.NORTH);
         frame.add(botPanel, BorderLayout.SOUTH);
         frame.add(leftPanel, BorderLayout.WEST);
         frame.add(rightPanel, BorderLayout.EAST);
         frame.add(centerPanel, BorderLayout.CENTER);
-
-
     }
 }
