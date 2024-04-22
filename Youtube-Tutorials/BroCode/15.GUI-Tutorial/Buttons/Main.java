fa766c9ba3ca56fc0e55dafaf7b96cbb2bd199eb
Hristo Yonkov <hristopturs@gmail.com>
2024-04-22 15:39:13 +0300

    Create Main.java

---
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/Buttons/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // JButton = A JAVA GUI button that performs an action when clicked on.
+
+        new MyFrame();
+
+    }
+}
