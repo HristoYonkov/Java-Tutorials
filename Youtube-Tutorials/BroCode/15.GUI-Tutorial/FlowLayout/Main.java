863794bfca0c07a058ca2b1dcacc635bd6e7f171
Hristo Yonkov <hristopturs@gmail.com>
2024-04-22 23:23:11 +0300

    Create Main.java

---
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/FlowLayout/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Layour Manager = Defines the natural layout for components within a container.
+        // FlowLayout = Places component in a row, sized at theit preferred size.
+        // If the horizontal space in the container is too small,
+        // the FlowLayout class uses the next available row.
+
+        
+    }
+}
