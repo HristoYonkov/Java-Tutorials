1e521fd488debddfe87dd09cd57579edc5d22146
Hristo Yonkov <hristopturs@gmail.com>
2024-04-23 10:45:02 +0300

    Update Main.java

--- Youtube-Tutorials/BroCode/15.GUI-Tutorial/FlowLayout/Main.java
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/FlowLayout/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
         // Layour Manager = Defines the natural layout for components within a container.
@@ -5,6 +7,10 @@ public class Main {
         // If the horizontal space in the container is too small,
         // the FlowLayout class uses the next available row.
 
-        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getSize(500, 500);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
     }
 }
