0c08a439e7c257567bde8b5d999c015834178f53
Hristo Yonkov <hristopturs@gmail.com>
2024-05-02 18:36:14 +0300

    Update MyFrame.java

--- Youtube-Tutorials/BroCode/15.GUI-Tutorial/JCheckBox/MyFrame.java
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/JCheckBox/MyFrame.java
@@ -1,7 +1,10 @@
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 
@@ -10,16 +13,36 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
-        JCheckBox checkbox = new JCheckBox();
+        xIcon = new ImageIcon("xIcon.png");
+        checkIcon = new ImageIcon("checkIcon.png");
 
+        button = new JButton();
+        button.setText("Submit");
+        button.addActionListener(this);
+
+        checkbox = new JCheckBox();
+        checkbox.setText("I'm not a robot!");
+        checkbox.setFocusable(false); // Remove the annoying border.
+        checkbox.setFont(new Font("Consolas", Font.PLAIN, 35));
+        checkbox.setIcon(xIcon); // Sets icon when unchecked.
+        checkbox.setSelectedIcon(checkIcon); // Sets icon when checked.
+
+        this.add(button);
         this.add(checkbox);
         this.pack();
         this.setVisible(true);
     }
 
+    JButton button;
+    JCheckBox checkbox;
+    ImageIcon xIcon;
+    ImageIcon checkIcon;
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        if (e.getSource() == button) {
+            System.out.println(checkbox.isSelected()); // Tells if the checkbox is selected (boolean) value.
+        }
     }
     
 }
