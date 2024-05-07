15c0cea06b10894a199eb55e5bcfbe046573ac99
Hristo Yonkov <hristopturs@gmail.com>
2024-05-07 13:13:53 +0300

    create myFrame class

---
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/RadioButtons/MyFrame.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyFrame extends JFrame implements ActionListener {
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
+    }
+
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+    
+}
