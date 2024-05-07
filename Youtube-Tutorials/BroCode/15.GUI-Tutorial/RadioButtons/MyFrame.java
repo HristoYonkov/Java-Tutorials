ef446ecbb9caed972373a3e4002ee37afe350dd8
Hristo Yonkov <hristopturs@gmail.com>
2024-05-07 15:06:41 +0300

    add logic for radio buttons

--- Youtube-Tutorials/BroCode/15.GUI-Tutorial/RadioButtons/MyFrame.java
+++ Youtube-Tutorials/BroCode/15.GUI-Tutorial/RadioButtons/MyFrame.java
@@ -1,4 +1,8 @@
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -6,16 +10,51 @@ public class MyFrame extends JFrame implements ActionListener {
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        pizzaIcon = new ImageIcon("pizza.png");
+        hamburgerIcon = new ImageIcon("hamburger.png");
+        hotdogIcon = new ImageIcon("hotdog.png");
+
+        pizzaButton = new JRadioButton("pizza");
+        hamburgerButton = new JRadioButton("hamburger");
+        hotdogButton = new JRadioButton("hotdog");
+        
+        pizzaButton.addActionListener(this);
+        hamburgerButton.addActionListener(this);
+        hotdogButton.addActionListener(this);
+        
+        pizzaButton.setIcon(pizzaIcon);
+        hamburgerButton.setIcon(hamburgerIcon);
+        hotdogButton.setIcon(hotdogIcon);
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(pizzaButton);
+        group.add(hamburgerButton);
+        group.add(hotdogButton);
+        
+        this.add(pizzaButton);
+        this.add(hamburgerButton);
+        this.add(hotdogButton);
         this.pack();
         this.setVisible(true);
     }
 
-    
+    JRadioButton pizzaButton;
+    JRadioButton hamburgerButton;
+    JRadioButton hotdogButton;
+    ImageIcon pizzaIcon;
+    ImageIcon hamburgerIcon;
+    ImageIcon hotdogIcon;
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        if (e.getSource() == pizzaButton) {
+            System.out.println("You ordered pizza.");
+        } else if (e.getSource() == hamburgerButton) {
+            System.out.println("You ordered hamburger.");
+        } else if (e.getSource() == hotdogButton) {
+            System.out.println("You ordered hotdog.");
+        }
     }
     
 }
